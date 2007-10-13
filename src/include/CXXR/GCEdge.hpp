b4b1eb07aadde66b873b51e24ff66862957f4564
Andrew Runnalls <a.r.runnalls@kent.ac.uk>
2007-10-13 16:55:21 +0000

    Erroneously copied comment removed.
    git-svn-id: http://projects.cs.kent.ac.uk/projects/cxxr/svn/trunk@133 549f2c98-2231-0410-b618-a09cd5e121c0

--- src/include/CXXR/GCEdge.hpp
+++ src/include/CXXR/GCEdge.hpp
@@ -17,17 +17,6 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-/* The following code is adapted from an example taken from the book
- * "The C++ Standard Library - A Tutorial and Reference"
- * by Nicolai M. Josuttis, Addison-Wesley, 1999
- *
- * (C) Copyright Nicolai M. Josuttis 1999.
- * Permission to copy, use, modify, sell and distribute this software
- * is granted provided this copyright notice appears in all copies.
- * This software is provided "as is" without express or implied
- * warranty, and with no claim as to its suitability for any purpose.
- */
-
 #ifndef GCEDGE_HPP
 #define GCEDGE_HPP 1
 
