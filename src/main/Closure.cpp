8bb1493dc84206fd48c894bb4c03f28ca3aa0a3a
Andrew Runnalls <a.r.runnalls@kent.ac.uk>
2008-05-02 09:55:02 +0000

    Forced creation of non-inline version of SET_CLOENV().
    git-svn-id: http://projects.cs.kent.ac.uk/projects/cxxr/svn/trunk@288 549f2c98-2231-0410-b618-a09cd5e121c0

--- src/main/Closure.cpp
+++ src/main/Closure.cpp
@@ -50,6 +50,7 @@ namespace CXXR {
 	SEXP (*CLOENVp)(SEXP x) = CLOENV;
 	SEXP (*FORMALSp)(SEXP x) = FORMALS;
 	int (*MISSINGp)(SEXP x) = MISSING;
+	void (*SET_CLOENVp)(SEXP x, SEXP v) = SET_CLOENV;
 	void (*SET_MISSINGp)(SEXP x, int v) = SET_MISSING;
 	void (*SET_DEBUGp)(SEXP x, Rboolean v) = SET_DEBUG;
     }
