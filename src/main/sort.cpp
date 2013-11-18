fbd5db1a4998e4cc875cdeb3a48940a8da20057c
Karl Millar <kmillar@google.com>
2013-11-18 05:53:41 +0000

    Fix buffer overflow bug in shellsort (seperately fixed in R 3.0.2)
    git-svn-id: http://projects.cs.kent.ac.uk/projects/cxxr/svn/branches/kmillar/trunk@1418 549f2c98-2231-0410-b618-a09cd5e121c0

--- src/main/sort.cpp
+++ src/main/sort.cpp
@@ -350,9 +350,9 @@ SEXP attribute_hidden do_sort(SEXP call, SEXP op, SEXP args, SEXP rho)
 
 /* faster versions of shellsort, following Sedgewick (1986) */
 
-static const int incs[16] = {1073790977, 268460033, 67121153, 16783361, 4197377,
+static const int incs[17] = {1073790977, 268460033, 67121153, 16783361, 4197377,
 		       1050113, 262913, 65921, 16577, 4193, 1073, 281, 77,
-		       23, 8, 1};
+		       23, 8, 1, 1};
 
 #define sort2_body \
     for (h = incs[t]; t < 16; h = incs[++t]) \
