366dc9d86c8ccac80f1789c090de4ee15f1f3f12
Karl Millar <kmillar@google.com>
2013-12-27 05:09:44 +0000

    Add missing 'explicit' qualification to constructor.
    git-svn-id: http://projects.cs.kent.ac.uk/projects/cxxr/svn/branches/kmillar/trunk@1437 549f2c98-2231-0410-b618-a09cd5e121c0

--- src/include/CXXR/FixedVector.hpp
+++ src/include/CXXR/FixedVector.hpp
@@ -87,7 +87,7 @@ namespace CXXR {
 	 * @param sz Number of elements required.  Zero is
 	 *          permissible.
 	 */
-	FixedVector(std::size_t sz)
+	explicit FixedVector(std::size_t sz)
 	    : VectorBase(ST, sz), m_data(singleton())
 	{
 	    if (sz > 1)
