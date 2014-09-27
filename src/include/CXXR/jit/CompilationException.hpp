5c8a79a50d192591219fe982eeb1c160ca63af4c
Karl Millar <kmillar@google.com>
2014-09-26 21:48:38 -0700

    Add exception class for JIT compilation errors.

---
+++ src/include/CXXR/jit/CompilationException.hpp
@@ -0,0 +1,58 @@
+/*CXXR $Id$
+ *CXXR
+ *CXXR This file is part of CXXR, a project to refactor the R interpreter
+ *CXXR into C++.  It may consist in whole or in part of program code and
+ *CXXR documentation taken from the R project itself, incorporated into
+ *CXXR CXXR (and possibly MODIFIED) under the terms of the GNU General Public
+ *CXXR Licence.
+ *CXXR 
+ *CXXR CXXR is Copyright (C) 2008-14 Andrew R. Runnalls, subject to such other
+ *CXXR copyrights and copyright restrictions as may be stated below.
+ *CXXR 
+ *CXXR CXXR is not part of the R project, and bugs and other issues should
+ *CXXR not be reported via r-bugs or other R project channels; instead refer
+ *CXXR to the CXXR website.
+ *CXXR */
+
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2.1 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, a copy is available at
+ *  http://www.r-project.org/Licenses/
+ */
+
+/** @file CompilationException.hpp
+ *
+ * @brief Exception class CXXR::JIT::CompilationException.
+ */
+
+#ifndef CXXR_JIT_COMPILATION_EXCEPTION_HPP
+#define CXXR_JIT_COMPILATION_EXCEPTION_HPP
+
+namespace CXXR {
+
+namespace JIT {
+    /** @brief Exception thrown when compilation fails.
+     *
+     * An exception of this class is thrown when an attempt to compile
+     * R code fails, typically because the code uses contructs that the
+     * compiler cannot reason about.
+     */
+    struct CompilationException {
+    };
+
+}  // namespace JIT
+}  // namespace CXXR
+
+#endif  // CXXR_JIT_COMPILATION_EXCEPTION_HPP
