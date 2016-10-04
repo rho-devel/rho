f5b31973c765ff2ad3f28c495887b466cdf938ea
Karl Millar <kmillar@google.com>
2016-10-03 17:04:00 -0700

    Fix the error message produced for unnamed symbols.

--- src/main/Symbol.cpp
+++ src/main/Symbol.cpp
@@ -121,7 +121,7 @@ RObject* Symbol::evaluate(Environment* env)
     if (val == unboundValue())
 	Rf_error(_("object '%s' not found"), name()->c_str());
     if (val == missingArgument() && !isDotDotSymbol()) {
-	if (m_name)
+	if (m_name && m_name != String::blank())
 	    Rf_error(_("argument \"%s\" is missing, with no default"),
 		     name()->c_str());
 	else Rf_error(_("argument is missing, with no default"));
